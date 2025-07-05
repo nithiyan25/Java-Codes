d62bcac3743090449029e695a6e140bfaca88d95
nithiyan25 <nithiyan2501@gmail.com>
2025-07-05 14:17:16 +0530

    java

--- SmithNumber.java
+++ SmithNumber.java
@@ -8,56 +8,50 @@ class SmithNumber {
         int primesum = 0;
         int temp=0;
         boolean isprime;
-        if (a==1){
-            isprime=true;
-        }
-        else{
-            for(int i=2;i<a;i++){
-                  if (a%i==0){
-                        temp+=1;
-                    }
+        if (a == 1) {
+            isprime = false;
+        } else {
+            for (int i = 2; i < a; i++) {
+                if (a % i == 0) {
+                    temp += 1;
+                }
             }
-            if (temp!=0){
-                isprime=true;
-            }
-            else{
-            isprime=false;
-                
+            if (temp == 0) {
+                isprime = true;
+            } else {
+                isprime = false;
             }
         }
         
-    if(isprime){
-        while (a!=0){
-            int d = a%10;
-            sum+=d;
-            a/=10;
+    if(!isprime){
+        while (a != 0) {
+            int d = a % 10;
+            sum += d;
+            a /= 10;
         }
-        a=n;
+        a = n;
         for (int i = 2; i <= n; i++) {
             while (n % i == 0) {
-                if (i>10){
-                    int j=i;
-                    while (j!=0){
-                    int d = j%10;
-                    primesum+=d;
-                    j/=10;
+                if (i >= 10) {
+                    int j = i;
+                    while (j != 0) {
+                        int d = j % 10;
+                        primesum += d;
+                        j /= 10;
                     }
-                }
-                else{
-                    primesum+=i;
+                } else {
+                    primesum += i;
                 }
                 n = n / i;
             }
         }
-            if (sum==primesum){
-                System.out.println("Smith Number :"+a);
-            }
-            else{
-                System.out.println("Not a Smith Number");
-            }
-    }
-    else{
-        System.out.println("Not a Smith Number(It is prime Number)");
+        if (sum == primesum) {
+            System.out.println("Smith Number :" + a);
+        } else {
+            System.out.println("Not a Smith Number");
+        }
+    } else {
+        System.out.println("Not a Smith Number (It is a prime number)");
     }
     input.close();
     }
